b0844bc65a9f2869a9c88f9c9b7f314786497f62
bobtwinkles <srkoser+github@gmail.com>
2014-11-10 13:05:56 -0500

    add ability to use non-system glm

--- cmake/FindGLM.cmake
+++ cmake/FindGLM.cmake
@@ -32,7 +32,8 @@
 # default search dirs
 SET(_glm_HEADER_SEARCH_DIRS
     "/usr/include"
-    "/usr/local/include")
+    "/usr/local/include"
+    "../lib/glm/")
 
 # check environment variable
 SET(_glm_ENV_ROOT_DIR "$ENV{GLM_ROOT_DIR}")
